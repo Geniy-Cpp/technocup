96f44b209d770a7baa17b6d93c884246190f30fd
Geniy-Cpp <59517561+Geniy-Cpp@users.noreply.github.com>
2020-08-16 01:41:20 +0300

    Add files via upload

---
+++ CppCoder.FirsOOP.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+		cout << "Введите количество запросов\n";
+	int iter, num, numOver = 0, need = 0;
+	cin >> iter;
+	system("cls");
+	for (int i = 0; i < iter; i++) {
+			cout << "Запрос номер " << i + 1 <<endl;
+			cout << "Введите некоторое количество спичек\n";
+			cin >> num;
+		numOver += num;
+		system("cls");
+	}
+	if (numOver % 2 == 0) {
+		need = 0;
+	}	
+	if (numOver == 2) {
+		need = 2;
+	}
+	if (numOver % 2 != 0) {
+		need = 1;
+	}
+	cout << "Необходимо докупить " << need << " спичек";
+	return 0;
+}
